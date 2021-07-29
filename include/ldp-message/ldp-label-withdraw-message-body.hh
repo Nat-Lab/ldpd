8d920c023b04b714c2b6ac34a4f17b69afdb362f
Nato Morichika <nat@nat.moe>
2021-07-29 16:42:36 -0400

    withdraw: fix

---
+++ include/ldp-message/ldp-label-withdraw-message-body.hh
@@ -0,0 +1,39 @@
+#ifndef LDP_LABEL_WITHDRAW_MESSAGE_H
+#define LDP_LABEL_WITHDRAW_MESSAGE_H
+#include <vector>
+#include "ldp-message/ldp-message-body.hh"
+#include "ldp-tlv/ldp-tlv.hh"
+
+namespace ldpd {
+
+class LdpLabelWithdrawMessageBody : public LdpMessageBody {
+public:
+    LdpLabelWithdrawMessageBody();
+    virtual ~LdpLabelWithdrawMessageBody();
+    uint16_t getType() const;
+
+    void setFecTlv(LdpRawTlv *tlv);
+    void setLabelTlv(LdpRawTlv *tlv);
+
+    const LdpRawTlv* getFecTlv() const;
+    const LdpRawTlv* getLabelTlv() const;
+
+private:
+    LdpRawTlv *_fec;
+    LdpRawTlv *_label;
+
+    uint8_t *_tmp_raw_buffer;
+    size_t _tmp_raw_buffer_sz;
+
+// ----------------------------------------------------------------------------
+
+public:
+    ssize_t parse(const uint8_t *from, size_t msg_sz);
+    ssize_t write(uint8_t *to, size_t buf_sz) const;
+    size_t length() const;
+};
+
+}
+
+
+#endif // LDP_LABEL_WITHDRAW_MESSAGE_H
--- src/ldp-message/ldp-lable-withdraw-message-body.cc
+++ src/ldp-message/ldp-lable-withdraw-message-body.cc
@@ -1,32 +1,41 @@
 #include "utils/log.hh"
-#include "ldp-message/ldp-lable-withdraw-message-body.hh"
+#include "ldp-message/ldp-label-withdraw-message-body.hh"
 
 #include <stdlib.h>
 
 namespace ldpd {
 
-LdpLableWithdrawMessageBody::LdpLableWithdrawMessageBody() : _fecs(), _labels() {
+LdpLabelWithdrawMessageBody::LdpLabelWithdrawMessageBody() {
+    _fec = nullptr;
+    _label = nullptr;
     _tmp_raw_buffer = nullptr;
     _tmp_raw_buffer_sz = 0;
 }
 
-LdpLableWithdrawMessageBody::~LdpLableWithdrawMessageBody() {
-    this->clearFecTlvs();
-    this->clearLabelTlvs();
+LdpLabelWithdrawMessageBody::~LdpLabelWithdrawMessageBody() {
+    if (_fec != nullptr) {
+        free(_fec);
+    }
+
+    if (_label != nullptr) {
+        free(_label);
+    }
 
     if (_tmp_raw_buffer != nullptr) {
         free(_tmp_raw_buffer);
     }
 }
 
+uint16_t LdpLabelWithdrawMessageBody::getType() const {
+    return 0x0402;
+}
+
 /**
- * @brief add fec tlv.
+ * @brief set fec tlv.
  * 
- * note: we handle freeing of tlv ourself. 
- * 
- * @param tlv tlv.
+ * @param tlv tlv - we will handle freeing this.
  */
-void LdpLableWithdrawMessageBody::addFecTlv(LdpRawTlv *tlv) {
+void LdpLabelWithdrawMessageBody::setFecTlv(LdpRawTlv *tlv) {
     uint16_t type = tlv->getType();
 
     if (type != 0x0100) {
@@ -34,17 +43,29 @@ void LdpLableWithdrawMessageBody::addFecTlv(LdpRawTlv *tlv) {
         return;
     }
 
-    _fecs.push_back(tlv);
+    if (_fec != nullptr) {
+        free(_fec);
+    }
+
+    _fec = tlv;
 }
 
 /**
- * @brief add lable tlv.
+ * @brief set label tlv.
  * 
- * note: we handle freeing of tlv ourself. 
- * 
- * @param tlv tlv.
+ * @param tlv tlv - we will handle freeing this. set to nullptr to remove lbl
+ * tlv.
  */
-void LdpLableWithdrawMessageBody::addLabelTlv(LdpRawTlv *tlv) {
+void LdpLabelWithdrawMessageBody::setLabelTlv(LdpRawTlv *tlv) {
+    if (tlv == nullptr) {
+        if (_label != nullptr) {
+            free(_label);
+        }
+
+        _label = nullptr;
+        return;
+    }
+
     uint16_t type = tlv->getType();
 
     if (type != 0x0200 && type != 0x0201 && type != 0x0202) {
@@ -52,64 +73,136 @@ void LdpLableWithdrawMessageBody::addLabelTlv(LdpRawTlv *tlv) {
         return;
     }
 
-    _labels.push_back(tlv);
-}
-
-/**
- * @brief clear all fec tlvs.
- * 
- */
-void LdpLableWithdrawMessageBody::clearFecTlvs() {
-    for (LdpRawTlv *tlv : _fecs) {
-        delete tlv;
+    if (_label != nullptr) {
+        free(_label);
     }
 
-    _fecs.clear();
-}
-
-/**
- * @brief clear all label tlvs.
- * 
- */
-void LdpLableWithdrawMessageBody::clearLabelTlvs() {
-    for (LdpRawTlv *tlv : _labels) {
-        delete tlv;
-    }
-
-    _fecs.clear();
+    _label = tlv;
 }
 
 /**
  * @brief get all fec tlvs.
  * 
- * @return const std::vector<LdpRawTlv *> tlvs.
+ * @return const LdpRawTlv* tlv.
  */
-const std::vector<LdpRawTlv *> LdpLableWithdrawMessageBody::getFecTlvs() const {
-    return _fecs;
+const LdpRawTlv* LdpLabelWithdrawMessageBody::getFecTlv() const {
+    return _fec;
 }
 
 /**
- * @brief get all label tlvs.
+ * @brief get all label tlv.
  * 
- * @return const std::vector<LdpRawTlv *> tlvs.
+ * @return const LdpRawTlv* tlv.
  */
-const std::vector<LdpRawTlv *> LdpLableWithdrawMessageBody::getLabelTlvs() const {
-    return _labels;
+const LdpRawTlv* LdpLabelWithdrawMessageBody::getLabelTlv() const {
+    return _label;
 }
 
 /**
  * @brief parse a withdraw message.
  * 
- * @param from source buffer.
- * @param msg_sz msg size from the msg hdr.
+ * @param from source buffer - must start after message id field.
+ * @param msg_sz msg size from the msg hd minus the size of msg id field.
  * @return ssize_t bytes read, or -1 on error.
  */
-ssize_t LdpLableWithdrawMessageBody::parse(const uint8_t *from, size_t msg_sz) {
+ssize_t LdpLabelWithdrawMessageBody::parse(const uint8_t *from, size_t msg_sz) {
+    const uint8_t *ptr = from;
+    size_t buf_remaining = msg_sz;
 
+    LdpRawTlv *fec = new LdpRawTlv();
+
+    ssize_t ret = fec->parse(from, buf_remaining);
+
+    if (ret < 0) {
+        delete fec;
+        return -1;
+    }
+
+    buf_remaining -= ret;
+    ptr += ret;
+
+    this->setFecTlv(fec);
+
+    LdpRawTlv *lbl = new LdpRawTlv();
+    ret = lbl->parse(from, msg_sz);
+
+    if (ret < 0) {
+        delete lbl;
+        return -1;
+    }
+
+    this->setLabelTlv(lbl);
+
+    buf_remaining -= ret;
+    ptr += ret;
+
+    if (buf_remaining != 0) {
+        log_fatal("reached the end but buf not end (%zu over).\n", buf_remaining);
+        return -1;
+    }
+
+    return ptr - from;
 }
 
 
-ssize_t write(uint8_t *to, size_t buf_sz) const;
-size_t length() const;
+/**
+ * @brief write message body - not incl msg id.
+ * 
+ * @param to dst buffer
+ * @param buf_sz dst buffer size.
+ * @return ssize_t bytes written, or -1 on error.
+ */
+ssize_t LdpLabelWithdrawMessageBody::write(uint8_t *to, size_t buf_sz) const {
+    uint8_t *ptr = to;
+    size_t buf_remaining = buf_sz;
+
+    if (_fec == nullptr) {
+        log_fatal("fec not set - not valid.\n");
+        return -1;
+    }
+
+    ssize_t ret = _fec->write(to, buf_remaining);
+
+    if (ret < 0) {
+        return -1;
+    }
+
+    buf_remaining -= ret;
+    ptr += ret;
+
+    if (_label == nullptr) {
+        return ptr - to;
+    }
+
+    ret = _label->write(to, buf_remaining);
+
+    if (ret < 0) {
+        return -1;
+    }
+
+    buf_remaining -= ret;
+    ptr += ret;
+
+    return ptr - to;
+}
+
+/**
+ * @brief get length of the body - no incl. msg id.
+ * 
+ * @return size_t length.
+ */
+size_t LdpLabelWithdrawMessageBody::length() const {
+    size_t len = 0;
+
+    if (_fec != nullptr) {
+        len += _fec->length();
+    }
+
+    if (_label != nullptr) {
+        len += _label->length();
+    }
+
+    return len;
+}
 
 }
