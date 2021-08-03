8b12680d6cbffd83d86d5ea818f4066155bf2d82
Nato Morichika <nat@nat.moe>
2021-08-03 01:37:46 -0400

    ldpd: handle exit

--- tests/ldpd.cc
+++ tests/ldpd.cc
@@ -1,7 +1,17 @@
 #include "core/ldpd.hh"
 #include "ldp-router/netlink-router.hh"
 #include "abstraction/router.hh"
+
 #include <arpa/inet.h>
+#include <signal.h>
+
+ldpd::Ldpd *d = nullptr;
+
+void handle_sig(__attribute__((unused)) int sig) {
+    if (d != nullptr) {
+        d->stop();
+    }
+}
 
 int main() {
     uint32_t rid = inet_addr("10.16.0.3");
@@ -9,6 +19,10 @@ int main() {
 
     ldpd::NetlinkRouter rtr = ldpd::NetlinkRouter();
     ldpd::Ldpd ldpd = ldpd::Ldpd(rid, 0, (ldpd::Router *) &rtr);
+
+    d = &ldpd;
+
+    signal(SIGINT, handle_sig);
 
     ldpd.setTransportAddress(ta);
     ldpd.start();
