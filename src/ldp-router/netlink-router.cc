c828f9b6c3c9d06aa18e3d51d2ac589bb6b77369
Nato Morichika <nat@nat.moe>
2021-08-02 22:34:36 -0400

    log, and logic fixes

--- src/ldp-router/netlink-router.cc
+++ src/ldp-router/netlink-router.cc
@@ -1,10 +1,17 @@
 #include "ldp-router/netlink-router.hh"
+#include <arpa/inet.h>
 
 namespace ldpd {
 
 NetlinkRouter::NetlinkRouter() : _nl(), _rib(), _rib_pending_del(), _fib() {
-    _nl.open();
+    log_debug("opening netlink services...\n");
+    
+    if (_nl.open() < 0) {
+        return;
+    }
+
     fullSync();
+    
     _nl.onIpv4RouteChanges(&NetlinkRouter::onRouteChange, this);
     _nl.onMplsRouteChanges(&NetlinkRouter::onRouteChange, this);
 }
@@ -15,12 +22,14 @@ NetlinkRouter::~NetlinkRouter() {
 
         if (route->getType() == RouteType::Mpls) {
             MplsRoute *r = (MplsRoute *) route;
-            _nl.addRoute(*r);
+            log_debug("(mpls) deleting route to %u from fib...\n", r->in_label);
+            _nl.deleteRoute(*r);
         }
 
         if (route->getType() == RouteType::Ipv4) {
             Ipv4Route *r = (Ipv4Route *) route;
-            _nl.addRoute(*r);
+            log_debug("(ipv4) deleting route to %s/%u from fib...\n", inet_ntoa(*(struct in_addr *) &(r->dst)), r->dst_len);
+            _nl.deleteRoute(*r);
         }
     }
 
@@ -65,6 +74,8 @@ void NetlinkRouter::fullSync() {
 }
 
 void NetlinkRouter::fetchFib() {
+    log_debug("performing full fib sync...\n");
+
     std::vector<Ipv4Route> v4 = std::vector<Ipv4Route>();
     std::vector<MplsRoute> m = std::vector<MplsRoute>();
 
@@ -86,20 +97,33 @@ void NetlinkRouter::fetchFib() {
 }
 
 void NetlinkRouter::pushRib() {
-    for (std::vector<Route *>::iterator i = _rib_pending_del.begin(); i != _rib_pending_del.end(); i = _rib_pending_del.erase(i)) {
+    for (std::vector<Route *>::iterator i = _rib_pending_del.begin(); i != _rib_pending_del.end();) {
         Route *route = *i;
 
         if (route->getType() == RouteType::Mpls) {
             MplsRoute *r = (MplsRoute *) route;
-            _nl.deleteRoute(*r);
+            log_debug("(mpls) deleting route to %u from fib...\n", r->in_label);
+            if (_nl.deleteRoute(*r) == 0) {
+                handleFibUpdate(NetlinkChange::Deleted, *r);
+                delete route;
+                i = _rib_pending_del.erase(i);
+                continue;
+            }
         }
 
         if (route->getType() == RouteType::Ipv4) {
             Ipv4Route *r = (Ipv4Route *) route;
-            _nl.deleteRoute(*r);
+            log_debug("(ipv4) deleting route to %s/%u from fib...\n", inet_ntoa(*(struct in_addr *) &(r->dst)), r->dst_len);
+            if (_nl.deleteRoute(*r) == 0) {
+                handleFibUpdate(NetlinkChange::Deleted, *r);
+                delete route;
+                i = _rib_pending_del.erase(i);
+                continue;
+            }
         }
 
-        delete route;
+        log_error("failed to delete route - will retry.\n");
+        ++i;
     }
 
     for (std::pair<uint64_t, Route *> r : _rib) {
@@ -111,17 +135,21 @@ void NetlinkRouter::pushRib() {
 
         if (route->getType() == RouteType::Mpls) {
             MplsRoute *r = (MplsRoute *) route;
+            log_debug("(mpls) adding route to %u tp fib...\n", r->in_label);
             _nl.addRoute(*r);
         }
 
         if (route->getType() == RouteType::Ipv4) {
             Ipv4Route *r = (Ipv4Route *) route;
+            log_debug("(ipv4) adding route to %s/%u to fib...\n", inet_ntoa(*(struct in_addr *) &(r->dst)), r->dst_len);
             _nl.addRoute(*r);
         }
     }
 }
 
 void NetlinkRouter::tick() {
+    _nl.tick();
+
     // todo: rate-limit this?
     pushRib();
 }
